c3920b15679cff88dddff3a7c6f1150bffe3d8ce
bjafl-sps <bjarte@sanpro.no>
2024-01-19 15:59:50 +0100

    The box i'm writing in now nees to contain some text. Usually it describes the changes that has been made. It's not important what you write here, but note that tis field needs to contain some text for the sync to work.
    Click plus sign on the change line below to attach this message to that change.
    
    Now the change (the filename..) has moved to staged changes.
    
    Next step is to click commit.
    Then you click sync, like this: ---

--- tasks/01.py
+++ tasks/01.py
@@ -7,6 +7,7 @@ def max(x, y):
 print(max(6, 5))
 
 # Hi! Now i'll show you how to upload the changes to your repo / fork
+
 # As you can see I've already made my solution and stored it in the tasks 
-# folder. Now I need to upload it. I start by going to source
-# control in the menu to the left....
+# folder. Now I need to upload it. 
+# I start by going to source control in the menu to the left....
